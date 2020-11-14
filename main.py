0c08cc0be4d07f50d976a5c5fb93cf4f26fbed42
Zolnowski <60990760+Zolnowski@users.noreply.github.com>
2020-11-14 22:42:31 +0100

    Update pxt.json, main.blocks, main.ts, main.py, tilemap.g.jres, tilemap.g.ts

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+@namespace
+class SpriteKind:
+    asteroid = SpriteKind.create()
 
+def on_a_pressed():
+    for value in sprites.all_of_kind(SpriteKind.asteroid):
+        value.set_position(randint(0, scene.screen_width()),
+            randint(0, scene.screen_height()))
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
--- main.ts
+++ main.ts
@@ -1,3 +1,29 @@
-forever(function () {
-	
+namespace SpriteKind {
+    export const asteroid = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    for (let value of sprites.allOfKind(SpriteKind.asteroid)) {
+        value.setPosition(randint(0, scene.screenWidth()), randint(0, scene.screenHeight()))
+    }
 })
+let mySprite: Sprite = null
+for (let index = 0; index < 10; index++) {
+    mySprite = sprites.create(img`
+        ...........fffffff...ccfff..........
+        ..........fbbbbbbbffcbbbbf..........
+        ..........fbb111bbbbbffbf...........
+        ..........fb11111ffbbbbff...........
+        ..........f1cccc1ffbbbbbcff.........
+        ..........ffc1c1c1bbcbcbcccf........
+        ...........fcc3331bbbcbcbcccf..ccccc
+        ............c333c1bbbcbcbccccfcddbbc
+        ............c333c1bbbbbbbcccccddbcc.
+        ............c333c11bbbbbccccccbbcc..
+        ...........cc331c11bbbbccccccfbccf..
+        ...........cc13c11cbbbcccccbbcfccf..
+        ...........c111111cbbbfdddddc.fbbcf.
+        ............cc1111fbdbbfdddc...fbbf.
+        ..............cccfffbdbbfcc.....fbbf
+        ....................fffff........fff
+        `, SpriteKind.asteroid)
+}
